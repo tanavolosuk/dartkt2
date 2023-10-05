b53f99d2c1e8cfeafdfcdf8a892ba34407aa5a7c
Tatiana Volosyuk <tanavolosuk92@gmail.com>
2023-10-05 11:38:12 +0500

    first

---
+++ bin/dartkt2.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+void main(List<String> arguments) async{
+  
+
+
+
+  // String srt = "s1000.00";
+  // double prices = double.parse(srt.substring(1));
+  // print(prices);
+  // print(prices.runtimeType);
+}
---
+++ test/dartkt2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartkt2/dartkt2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
